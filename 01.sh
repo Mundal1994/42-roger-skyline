78fcd017e2315c1b8c8b03580760234a1121abe5
molesen <molesen@debian.lan>
2022-04-22 15:53:47 +0100

    changes to 01 script

--- 01.sh
+++ 01.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cat /dev/null > /var/log/update_script.log
+echo "----------" >> /var/log/update_script.log
 date >> /var/log/update_script.log
-echo "begin update" >> /var/log/update_script.log
+echo " " >> /var/log/update_script.log
 apt-get update -y  >> /var/log/update_script.log 2>&1
-echo "end update" >> /var/log/update_script.log
-echo "begin upgrade" >> /var/log/update_script.log
 apt-get upgrade -y >> /var/log/update_script.log 2>&1
-echo "end upgrade" >> /var/log/update_script.log
+apt-get install --only-upgrade ufw >> /var/log/update_script.log 2>&1
+apt-get install --only-upgrade sudo >> /var/log/update_script.log 2>&1
+apt-get install --only-upgrade fail2ban >> /var/log/update_script.log 2>&1
+apt-get install --only-upgrade portsentry >> /var/log/update_script.log 2>&1
