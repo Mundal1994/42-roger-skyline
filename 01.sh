092714b50ba4daa4465fb992ed8a7ae139d535f6
molesen <molesen@debian.lan>
2022-04-26 09:47:11 +0100

    changes to update script

--- 01.sh
+++ 01.sh
@@ -9,3 +9,6 @@ apt-get install --only-upgrade ufw >> /var/log/update_script.log 2>&1
 apt-get install --only-upgrade sudo >> /var/log/update_script.log 2>&1
 apt-get install --only-upgrade fail2ban >> /var/log/update_script.log 2>&1
 apt-get install --only-upgrade portsentry >> /var/log/update_script.log 2>&1
+apt-get install --only-upgrade bsd-mailx >> /var/log/update_script.log 2>&1
+apt-get install --only-upgrade postfix >> /var/log/update_script.log 2>&1
+apt-get install --only-upgrade mutt >> /var/log/update_script.log 2>&1
--- 02.sh
+++ 02.sh
@@ -2,12 +2,14 @@
 C_FILE="/etc/crontab"
 FILE="/root/mail/prev_tab"
 DIF="$(diff $C_FILE $FILE)"
+CUR_DATE="$(date | awk '{print $2,$3,$6}')"
+MOD_DATE="$(date -d "@$(stat -c '%Y' /etc/crontab)" | awk '{print $2,$3,$6}')"
 
 if [ ! -f "$FILE" ]; then
 	: > "$FILE"
 fi
 
-if ! cmp -s "$C_FILE" "$FILE"; then
+if [ "$CUR_DATE" == "$MOD_DATE" ]; then
 	echo "$DIF" | sudo mail -s "crontab modification" root@debian.lan
 	cp "$C_FILE" "$FILE"
 fi
