41b7b32152437b05f8d4b29dfc5f39c84009d938
molesen <molesen@student.hive.fi>
2022-05-17 09:52:47 +0300

    updated 01 and 02 script

--- 01.sh
+++ 01.sh
@@ -4,7 +4,7 @@ echo "----------" >> /var/log/update_script.log
 date >> /var/log/update_script.log
 echo " " >> /var/log/update_script.log
 apt-get update -y  >> /var/log/update_script.log 2>&1
-apt-get upgrade -y >> /var/log/update_script.log 2>&1
+apt-get --with-new-pkgs upgrade -y >> /var/log/update_script.log 2>&1
 apt-get install --only-upgrade ufw >> /var/log/update_script.log 2>&1
 apt-get install --only-upgrade sudo >> /var/log/update_script.log 2>&1
 apt-get install --only-upgrade fail2ban >> /var/log/update_script.log 2>&1
--- 02.sh
+++ 02.sh
@@ -2,14 +2,12 @@
 C_FILE="/etc/crontab"
 FILE="/root/mail/prev_tab"
 DIF="$(diff $C_FILE $FILE)"
-CUR_DATE="$(date | awk '{print $2,$3,$6}')"
-MOD_DATE="$(date -d "@$(stat -c '%Y' /etc/crontab)" | awk '{print $2,$3,$6}')"
 
 if [ ! -f "$FILE" ]; then
 	: > "$FILE"
 fi
 
-if [ "$CUR_DATE" == "$MOD_DATE" ]; then
+if [ $(( $(date +%s) - $(date +%s -r /etc/crontab) )) -le 86400 ]; then
 	echo "$DIF" | sudo mail -s "crontab modification" root@debian.lan
 	cp "$C_FILE" "$FILE"
 fi
